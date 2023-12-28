dd0595c6430c51be375213f6c78b1c30f4fed352
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2023-12-28 19:40:18 +0530

    comments.py
    using comments in python

---
+++ comments.py
@@ -0,0 +1,19 @@
+# Hey, please don't remove this line
+# this is a one line comment
+"""
+Author: aryan 
+"""
+# using  three double quotes for commenting in python
+
+'''
+Author: aryan
+'''
+# using three single quotes for commenting in python
+
+print("Hey I am a good boy \n and this viewer is also a good boy/girl ")
+
+# using /n for a new line in python
+
+# print("Hey I am a "good boy" ")  THIS IS WRONG WAY
+print("Hey I am a \"good boy\" ") 
+# THIS IS CORRECT WAY
