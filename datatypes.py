a80573727acc50dcd90d6911a7acb9fbf56bc125
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2023-12-29 18:39:06 +0530

    datatypes.py
    Basic knowledge about the variables and datatypes in python language.

---
+++ datatypes.py
@@ -0,0 +1,37 @@
+a=212    #integer
+b=5485.54   #float
+c=complex(8,2)   #complex no.
+d=True    #Boolean
+e="aryan"   #string
+f=None    #none
+g=127
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+print("The type of a is ",type(a))
+print("The type of a is ",type(b))
+print("The type of a is ",type(c))
+print("The type of a is ",type(d))
+print("The type of a is ",type(e))
+print("The type of a is ",type(f))
+
+print(a+b)  #These are valid concatenations
+print(a+g)
+print(a+c)
+print(a+d)
+# print(a+e) , print(a+f) INVALID
+
+list=[8,2.3,[-21,3],["apple","banana"]]
+print(list)
+
+tuple=(("parrot","sparrow"),("lion","tiger"))
+print(tuple)
+
+dict={"name":"Tommy","age":"20","canVote":True}   #Dictionary
+print(dict)
+
