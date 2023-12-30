cfb41af93a2c07c3b29ee68e2ff9e8f4980984c9
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2023-12-30 22:53:16 +0530

    calculator.py
    This is a very basic calculator with the operands values already known to us.

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a=5
+b=3
+
+print("The value of",a,"+",3,'is:',a+3)     #ADDITION
+print("The value of",a,"-",3,'is:',a-3)     #SUBTRACTION
+print("The value of",a,"*",3,'is:',a*3)     #MULTIPLICATION
+print("The value of",a,"/",3,'is:',a/3)     #DIVISION
+print("The value of",a,"//",3,'is:',a//3)   #Removes the decimal part
+print("The value of",a,"**",3,'is:',a**3)   #EXPONENTIAL
