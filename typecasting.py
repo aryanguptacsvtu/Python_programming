41c35495979377fbed4eecdca34c1acd9420b25f
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2023-12-30 23:03:53 +0530

    typecasting.py
    Using typecasting in python

---
+++ typecasting.py
@@ -0,0 +1,10 @@
+a="1"       #a and b are strings
+b="2"
+
+print(int(a)+int(b))  #EXPLICIT Typecasting
+
+c=3.2
+d=12
+print(c+d)    #IMPLICIT Typecasting
+
+print(type(a),'\n',type(c),'\n',type(d))
