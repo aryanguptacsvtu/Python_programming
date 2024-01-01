fb3a7868cef364a939c55edf12a2ebef52c7e7ac
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-01-01 19:04:31 +0530

    TakingInput.py
    Taking user input in python.

---
+++ TakingInput.py
@@ -0,0 +1,12 @@
+a=input("Enter your name:")
+print("My name is",a)
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+
+print(int(x)+int(y))
+print(float(x)-float(y))
+print(float(x)*float(y))
+print(int(x)/int(y))
+print(int(x)//int(y))
+print(int(x)%int(y))
