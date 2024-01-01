f86b5199b1a6d1a64057b8dbe863aae28d7b5f7c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-01-01 19:26:12 +0530

    strings.py
    String in python

---
+++ strings.py
@@ -0,0 +1,22 @@
+name ='Harry'
+friend = "Rohan"
+
+#Multiple line strings using triple inverted commas
+apple ='''He said,         
+hi harry,                      
+"I want to eat an apple." '''
+
+print("Hello "+name)
+print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) Throws an error!
+
+print("Let's use a for loop")
+for character in apple:
+    print(character)
+
