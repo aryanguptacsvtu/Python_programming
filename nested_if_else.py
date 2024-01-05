336b0d9413f96245640c632c12acfcb0d72af049
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-01-05 20:38:49 +0530

    nested_if_else.py
    Nesting of the if, else loops in PYTHON.

---
+++ nested_if_else.py
@@ -0,0 +1,14 @@
+num =int(input("Enter the value of num:"))
+
+if(num<0):
+    print("Number is negative.")
+elif(num>0):
+    if(num<=10):
+     print("Number is between 1-10.")   # Nesting of the loops.
+    elif(num>10 and num<=20):
+     print("Number is between 11-20.")
+    else:
+     print("Number is greater than 20.")
+
+else:
+   print("Number is zero.")
