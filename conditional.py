5f603b70279dd3239e8a1ae7e3023bb70567dd49
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-01-05 20:29:51 +0530

    conditional.py
    Knowing about the Conditional loops( if, elif, else) in PYTHON.

---
+++ conditional.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the value of num:"))
+
+if(num<0):
+    print("Number is negative.")
+elif(num==0):
+    print("Number is zero.")
+else:
+    print("Number is positive.")
+
+print("I am happy now.")  #This is out of the loops.
