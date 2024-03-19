0b6dd1c4b57e58cac35d0e246710660c8f9cdc7d
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-03-19 18:23:07 +0530

    doc_strings.py
    Knowing about the "Doc strings" in Python.

---
+++ doc_string.py
@@ -0,0 +1,10 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+# Docstrings are strings used right after the definition of a function,method,class or module.
+# They are descriptions that help to better understand and improve functionality of the program. 
+
+
