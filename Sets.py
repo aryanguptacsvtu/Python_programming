a00fe85fafa390e6eff26eeeafa918e0a0e53d78
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-03-20 12:05:00 +0530

    Sets.py
    Knowing about "SETS" in Python.

---
+++ Sets.py
@@ -0,0 +1,15 @@
+# Sets are unchangeable & do not contain duplicate items.
+# Items of set occur in random manner.
+
+s={2,4,67,2,5}
+print(s)
+
+info={"carla",19,False,5.9,19}
+print(info)
+
+harry=set()             # EMPTY SET
+print(type(harry))
+
+for value in info:
+    print(value)
+    
